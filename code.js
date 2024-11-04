99516f5c4475058d18d5636a2683bef834e0eff0
karolinarm08 <159954535+karolinarm08@users.noreply.github.com>
2024-11-04 11:03:00 +0200

    rename

---
+++ code.js
@@ -0,0 +1,28 @@
+// Получить кнопку
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    
+// Показать кнопку, когда пользователь прокрутил вниз на 100 пикселей
+window.onscroll = function () {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+// При нажатии на кнопку прокрутить вверх
+scrollToTopBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Плавная прокрутка
+    });
+};
+
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
